4d484aedb8841ede3385c697d138c0778670e6c3
Fayyaz-WebPenter <jamfayyaz298@gmail.com>
2026-05-09 17:58:30 +0500

    some improment

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "tailors");
+$conn = mysqli_connect("localhost", "root", "P@ssw0rd123!", "tailors");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
